b4f4a295922648c8958a065b5189873f5b3b48ff
RajShekar-Bommalata <37531427+RajShekar-Bommalata@users.noreply.github.com>
2018-12-13 12:18:43 +0530

    Update locator_builder.js
    modified name , class, id locators

--- locator_builder.js
+++ locator_builder.js
@@ -1,5 +1,8 @@
 var element = $0;
-var temp_element = element.outerHTML;
+var temp_element = element.cloneNode().outerHTML;
+var locatorsCount = 0;
+var locatorsList = {};
+var selector="";
 var elementType = temp_element.substring(1, temp_element.indexOf(" "));
 var element_has_class = temp_element.includes("class=");
 var element_has_id = temp_element.includes("id=");
@@ -7,19 +10,34 @@ var element_has_name = temp_element.includes("name=");
 var element_has_href = temp_element.includes("href=");
 var element_has_for = temp_element.includes("for=");
 var element_has_alt = temp_element.includes("alt=");
+var element_has_src = temp_element.includes("src=");
+var element_has_style = temp_element.includes("style=");
 
 
 if (element_has_class) {
-    console.log('class="' + element.getAttribute("class") + '"');
-    console.log('//' + elementType + '[@class="' + element.getAttribute("class") + '"]');
+    selector='class=' + element.getAttribute("class");
+console.log(selector);
+    locatorsList.class={type:"class",locator : selector,occurance:1,suggestion:"true green"}
+    selector ='//' + elementType + '[@class="' + element.getAttribute("class") + '"]';
+console.log(selector);
+    locatorsList.class={type:"class",locator : "selector",occurance:1,suggestion:"true green"}
+    
 }
-if (element_has_id) {
-    console.log('id=' + element.getAttribute("id"));
-    console.log('//' + elementType + '[@id="' + element.getAttribute("id") + '"]');
+if (element_has_id && element_has_id != "") {
+    selector='id=' + element.getAttribute("id");
+console.log(selector);
+    locatorsList.id={type:"id",locator : selector,occurance:1,suggestion:"true yellow"}
+selector ='//' + elementType + '[@id="' + element.getAttribute("id") + '"]';
+console.log(selector);
 }
 if (element_has_name) {
+selector='name=' + element.getAttribute("name");
+console.log(selector);
     //console.log("name=" + element.getAttribute("name"));
-    console.log('//' + elementType + '[@name="' + element.getAttribute("name") + '"]');
+    selector='//' + elementType + '[@name="' + element.getAttribute("name") + '"]';
+console.log(selector);
+    locatorsList.name={type:"name",locator : selector,occurance:1,suggestion:"true green"}
+
 }
 if (element_has_href) {
     //console.log("href=" + element.getAttribute("href"));
@@ -33,3 +51,11 @@ if (element_has_alt) {
     //console.log("alt=" + element.getAttribute("alt"));
     console.log('//' + elementType + '[@alt="' + element.getAttribute("alt") + '"]');
 }
+if (element_has_src) {
+    console.log('//' + elementType + '[@src="' + element.getAttribute("src") + '"]');
+}
+if (element_has_style) {
+    console.log('//' + elementType + '[@style="' + element.getAttribute("style") + '"]');
+}
+console.log(locatorsList.length);
+
