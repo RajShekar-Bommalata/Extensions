89681118581a5a0d05773aa8d4b26e7893316282
RajShekar-Bommalata <37531427+RajShekar-Bommalata@users.noreply.github.com>
2018-12-18 00:55:32 +0530

    deleted HQaS

--- Headless_QaSCRIBE/QaSCRIBE/content/headless/extension-info.js
+++
@@ -1,9 +0,0 @@
-/**
- * eureQa team added code 
- * 
- * This code will add the currently running qascribe extension details to page DOM
- */
-$('<script>var _HeadlessQascribe_Info = _HeadlessQascribe_Info || {};\
-    _HeadlessQascribe_Info.version = "'+browser.runtime.getManifest().version+'";\
-    </script>').appendTo(document.body);
-    
